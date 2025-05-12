faf268aca50228ef33b3bae9ecd064b14f66f469
노현지 <shguswl826@gmail.com>
2025-05-12 16:58:23 +0900

    login/signup 서버 연결결

---
+++ dev/router/auth.mjs
@@ -0,0 +1,38 @@
+import express from "express";
+import * as authController from "../controller/auth.mjs";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.mjs";
+
+const router = express.Router();
+
+// 아이디 조건 통과 -> 비밀번호 조건 통과 -> validate로 통과
+const validateLogin = [
+  body("userid")
+    .trim()
+    .isLength({ min: 4 })
+    .withMessage("최소 4자 이상 입력")
+    .matches(/^[a-zA-Z0-9]*$/)
+    .withMessage("특수문자는 사용불가"),
+  body("password")
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage("최소 8자 이상 입력"),
+  validate,
+];
+
+const validateSignup = [
+  ...validateLogin,
+  body("name").trim().notEmpty().withMessage("이름 입력"),
+  body("email").trim().isEmail().withMessage("이메일 형식 확인"),
+  validate,
+];
+
+// 회원가입
+router.post("/signup", validateSignup, authController.signup);
+
+// 로그인
+router.post("/login", validateLogin, authController.login);
+
+// 로그인 유지
+
+export default router;
