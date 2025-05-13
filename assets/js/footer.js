e44749dfc741b98aa24fd4282fb5c7094b083b7e
오지환 <ovitwisdom@gmail.com>
2025-05-14 01:11:46 +0900

    페이지 하나 정리

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const toggleBtn = document.getElementById("familyToggleBtn");
-  const dropdownList = document.getElementById("familySiteList");
+	const toggleBtn = document.getElementById("familyToggleBtn");
+	const dropdownList = document.getElementById("familySiteList");
 
-  toggleBtn.addEventListener("click", function (e) {
-    e.stopPropagation();
-    dropdownList.classList.toggle("show");
-  });
+	toggleBtn.addEventListener("click", function (e) {
+		e.stopPropagation();
+		dropdownList.classList.toggle("show");
+	});
 
-  // 외부 클릭 시 닫기
-  window.addEventListener("click", function (e) {
-    if (!e.target.closest(".family-site-wrapper")) {
-      dropdownList.classList.remove("show");
-    }
-  });
+	// 외부 클릭 시 닫기
+	window.addEventListener("click", function (e) {
+		if (!e.target.closest(".family-site-wrapper")) {
+			dropdownList.classList.remove("show");
+		}
+	});
 });
